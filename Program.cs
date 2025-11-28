6ed5609c2cc34585d89c1070409e47c5146423f9
Zennoon <98653096+Zennoon@users.noreply.github.com>
2025-11-28 20:26:31 +0300

    Setup Swagger for API documentation and testing

--- Program.cs
+++ Program.cs
@@ -1,5 +1,28 @@
+using Microsoft.OpenApi;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(config =>
+{
+    config.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "PizzaStoreInMemory API",
+        Description = "Making the pizzas you love!",
+        Version = "v1"
+    }
+    );
+});
+
 var app = builder.Build();
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(config =>
+    {
+        config.SwaggerEndpoint("/swagger/v1/swagger.json", "PizzaStoreInMemory API v1");
+    });
+}
 
 app.MapGet("/", () => "Hello World!");
 
